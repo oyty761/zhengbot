35301199c545d4c81dbed0430acd2b9b3e3e6dea
LinklessKing <kel586673@gmail.com>
2026-08-05 21:02:29 +0800

    JPT数据库后的点歌模块修改

--- backend/src/main/java/com/guzheng/GuzhengApplication.java
+++ backend/src/main/java/com/guzheng/GuzhengApplication.java
@@ -3,7 +3,15 @@ package com.guzheng;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {
+        "com.guzheng.common",
+        "com.guzheng.config",
+        "com.guzheng.composition",
+        "com.guzheng.controller",
+        "com.guzheng.mapper",
+        "com.guzheng.service",
+        "com.guzheng.songbook"
+})
 public class GuzhengApplication {
 
     public static void main(String[] args) {
